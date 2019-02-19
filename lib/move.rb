a69b1e19cd39fa4c54bcb578009260b1881ad583
Donna Amos <donnacamos.developer@gmail.com>
2019-02-19 18:39:09 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,7 +8,7 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(user_input = ["1", "2", "3", "4", "5", "6", "7", "8","9"])
-def  user_input.to_i-1 
+def  user_input.to_i 
 end
 end
 
