a5b1a3b0c02673e44d870a9889437d11a05e3586
Donna Amos <donnacamos.developer@gmail.com>
2019-02-19 17:34:16 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input = ["1", "2", "3", "4", "5", "6", "7", "8","9"])
   (user_input).to_i-1
 end
 
-def move(user_input, index = 0, value = "X")
+def move(board, index = 0, value = "X")
 user_input[0] = value
 
 end
